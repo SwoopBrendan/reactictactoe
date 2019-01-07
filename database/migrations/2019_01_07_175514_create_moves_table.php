c23c30f3b892867e66df5e1a14e7af47d0f5eacc
brendansf <brendan@shoppingfeeder.com>
2019-01-07 20:00:33 +0200

    Created initial models and migrations

---
+++ database/migrations/2019_01_07_175514_create_moves_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMovesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('moves', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('game_id')->unsigned();
+            $table->integer('move_number');
+            $table->string('location');
+            $table->string('player');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('moves');
+    }
+}
